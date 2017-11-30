f5be176d3946f70d0e53cc6574a46eb888f09c26
Tycho van der Ouderaa <tychovdo@gmail.com>
2017-11-30 17:52:29 +0100

    run

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH -t 200
+#SBATCH -t 500
 #SBATCH -p gpu
-python main.py data/linux/train.txt data/linux/test.txt models/linux_2_3x512_0d2_lstm_50l_20000 --cuda --hidden_size 512 --dropout 0.2 --n_epochs 20000 --n_layers 3 --chunk_len 50 --rnn_class lstm --print_every 2000
+python main.py data/linux/train.txt data/linux/test.txt models/linux_3_3x512_0d5_lstm_50l_50000 --cuda --hidden_size 512 --dropout 0.2 --n_epochs 50000 --n_layers 3 --chunk_len 128 --rnn_class lstm --print_every 2000 >> log1.txt &
+python main.py data/linux/train.txt data/linux/test.txt models/linux_3_3x512_0d5_gru_50l_50000 --cuda --hidden_size 512 --dropout 0.2 --n_epochs 50000 --n_layers 3 --chunk_len 128 --rnn_class gru --print_every 2000 >> log2.txt
